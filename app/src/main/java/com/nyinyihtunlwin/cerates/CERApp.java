7314192c2119a49e3f95179dcfd69bde3d96148b
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2018-05-01 12:08:55 +0630

    UI updated.

---
+++ app/src/main/java/com/nyinyihtunlwin/cerates/CERApp.java
@@ -0,0 +1,10 @@
+package com.nyinyihtunlwin.cerates;
+
+import android.app.Application;
+
+public class CERApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
